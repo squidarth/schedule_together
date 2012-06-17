8c2d7fabbddb798274edfbdeddbcadd68450b06c
Sidharth Shanker <sshanker220@gmail.com>
2012-06-17 12:00:37 -0700

    Configuring unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
-APP_PATH = "/var/www/current"
+APP_PATH = "/var/www"
 stderr_path APP_PATH + "/log/unicorn.stderr.log"
 stdout_path APP_PATH + "/log/unicorn.stdout.log"
 pid APP_PATH + "/tmp/pid/unicorn.pid"
 worker_processes 2
-working_directory "/var/wwww/current"
+working_directory "/var/www" 
