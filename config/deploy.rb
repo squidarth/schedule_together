2183394ce8e2cbb7b9044bb3a1667700cb7ac1b3
Sidharth Shanker <sshanker220@gmail.com>
2012-06-17 01:41:22 -0700

    Updating the deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,6 +11,24 @@ set :deploy_to, "/var/www"
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 server 'ec2-23-22-250-175.compute-1.amazonaws.com', :web, :app, :db
+
+namespace :bundler do
+  task :create_symlink, :roles => :app do
+    shared_dir = File.join(shared_path, 'bundle')
+    release_dir = File.join(release_path, '.bundle')
+    run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
+  end
+  
+  task :install, :roles => :app do
+    run "cd #{release_path} && bundle install"
+  end
+
+  task :bundle_new_release, :roles => :db do
+    bundler.create_symlink
+    bundler.install
+  end
+end
+
 # if you're still using the script/reaper helper you will need
 #
 # these http://github.com/rails/irs_process_scripts
@@ -23,3 +41,6 @@ server 'ec2-23-22-250-175.compute-1.amazonaws.com', :web, :app, :db
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
 # end
+#
+after "deploy:rollback:revision", "bundler:install"
+after "deploy:update_code", "bundler:bundle_new_release"
