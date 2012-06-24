27cb9fb43769a31de057bd27938cb591e70a3b1d
Sidharth Shanker <sshanker220@gmail.com>
2012-06-23 20:30:46 -0700

    reconfiguring unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-APP_PATH = "/var/www"
+APP_PATH = "/var/www/current"
 stderr_path APP_PATH + "/log/unicorn.stderr.log"
 stdout_path APP_PATH + "/log/unicorn.stdout.log"
 pid APP_PATH + "/tmp/pids/unicorn.pid"
