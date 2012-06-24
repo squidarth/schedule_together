53a2ca74df2bf9392f0419d4bb71544a6bce04ed
Sidharth Shanker <sshanker220@gmail.com>
2012-06-23 20:52:54 -0700

    changed unicorn port

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,7 +2,7 @@ APP_PATH = "/var/www/current"
 stderr_path APP_PATH + "/log/unicorn.stderr.log"
 stdout_path APP_PATH + "/log/unicorn.stdout.log"
 pid APP_PATH + "/tmp/pids/unicorn.pid"
-listen 80
+listen 8080
 worker_processes 2
 working_directory "/var/www/current" 
 user "ubuntu"
