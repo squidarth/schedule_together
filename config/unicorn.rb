b9ad2b075fb02936c286f4e82255bccc4e067ef9
Sidharth Shanker <sshanker220@gmail.com>
2012-06-23 18:02:50 -0700

    Trying to get unicorn to work

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,11 +2,7 @@ APP_PATH = "/var/www"
 stderr_path APP_PATH + "/log/unicorn.stderr.log"
 stdout_path APP_PATH + "/log/unicorn.stdout.log"
 pid APP_PATH + "/tmp/pids/unicorn.pid"
-
-if Rails.env.production?
-  listen 80
-end
-
+listen 80
 worker_processes 2
-working_directory "/var/www" 
+working_directory "/var/www/current" 
 user "ubuntu"
